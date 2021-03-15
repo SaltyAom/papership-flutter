426e757004f4d71a08ef3a7f7f486d4ee04640ac
SaltyAom <aomkirby123@gmail.com>
2021-03-15 12:37:26 +0700

    :tada: feat - animation progress

--- lib/components/atoms/progress.dart
+++ lib/components/atoms/progress.dart
@@ -1,8 +1,10 @@
+// Flutter Hook + Niku
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 import 'package:niku/niku.dart';
 
-class Progress extends StatelessWidget {
+class Progress extends HookWidget {
   const Progress({
     Key key,
     @required this.progress,
@@ -15,13 +17,34 @@ class Progress extends StatelessWidget {
   final Color background;
 
   @override
-  Widget build(BuildContext context) {
-    return Container(color: color)
+  build(context) {
+    final double progression = progress.isNaN ? 1 : progress;
+
+    final animatedProgress = useAnimationController(
+      duration: const Duration(milliseconds: 500),
+      initialValue: progression,
+    );
+
+    useEffect(() {
+      animatedProgress.animateTo(progression, curve: Curves.easeOutBack);
+
+      return;
+    }, [progression]);
+
+    return ColoredBox(color: color)
         .niku()
-        .fractionWidth(progress.isNaN ? 1 : progress)
+        .builder(
+          (child) => AnimatedBuilder(
+            child: child,
+            animation: animatedProgress,
+            builder: (context, child) => FractionallySizedBox(
+              widthFactor: animatedProgress.value,
+              child: child,
+            ),
+          ),
+        )
         .height(3)
         .centerLeft()
-        .container()
         .bg(background)
         .fullWidth()
         .height(3)
@@ -29,3 +52,70 @@ class Progress extends StatelessWidget {
         .build();
   }
 }
+
+// Normal Flutter
+// import 'package:flutter/material.dart';
+
+// class Progress extends StatefulWidget {
+//   const Progress({
+//     Key key,
+//     @required this.progress,
+//     this.color = Colors.white,
+//     this.background = Colors.white54,
+//   }) : super(key: key);
+
+//   final double progress;
+//   final Color color;
+//   final Color background;
+
+//   @override
+//   _ProgressState createState() => _ProgressState();
+// }
+
+// class _ProgressState extends State<Progress>
+//     with SingleTickerProviderStateMixin {
+//   AnimationController animatedProgress;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     animatedProgress = AnimationController(
+//       duration: const Duration(milliseconds: 500),
+//       vsync: this,
+//     );
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     double progression = widget.progress.isNaN ? 1 : widget.progress;
+
+//     animatedProgress.animateTo(progression, curve: Curves.easeOutBack);
+
+//     return Container(
+//       height: 3,
+//       decoration: BoxDecoration(borderRadius: BorderRadius.circular(6)),
+//       child: FractionallySizedBox(
+//         widthFactor: 1,
+//         child: ColoredBox(
+//           color: widget.background,
+//           child: Align(
+//             alignment: Alignment.centerLeft,
+//             child: SizedBox(
+//               height: 3,
+//               child: AnimatedBuilder(
+//                 animation: animatedProgress,
+//                 builder: (context, child) => FractionallySizedBox(
+//                   widthFactor: animatedProgress.value,
+//                   child: child,
+//                 ),
+//                 child: ColoredBox(
+//                   color: widget.color,
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
